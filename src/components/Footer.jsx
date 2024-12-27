9d58524137f7ba925ddd6cb4c09ca8fcbf35defe
Axellerosh <lubi.axellerosh@gmail.com>
2024-12-27 19:00:50 +0800

    changing footer color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,9 @@ const socialLinks = [
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-[#5542ff] py-4 px-4 text-black">
+    <footer className="w-full bg-slate-900 py-4 px-4 ">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
-        <p className="text-center text-sm font-light md:text-left">
+        <p className="text-center text-sm font-light md:text-left text-white">
             @ 2024. All rights reserved
         </p>
 
@@ -22,7 +22,7 @@ const Footer = () => {
               href={link.href}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-black transition-colors duration-500 ease-in-out hover:text-white"
+              className="text-white transition-colors duration-500 ease-in-out hover:text-white"
             >
               {link.icon}
             </a>
@@ -31,7 +31,7 @@ const Footer = () => {
 
         <a
           href="#privacy-policy"
-          className="text-center text-sm font-light hover:underline md:text-right"
+          className="text-center text-sm font-light hover:underline md:text-right text-white"
         >
           XellTech
         </a>

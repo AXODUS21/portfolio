ec2e3e9e75824d0faf144bb72eb5faefd41663fe
Axellerosh <lubi.axellerosh@gmail.com>
2025-01-08 18:28:42 +0800

    trying to fix loading times

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -37,7 +37,7 @@ const BentoCard = ({src, title}) => {
     return (
       <div className="relative size-full ">
         <video
-          ref={nextVideoRef}
+          preload="metadata"
           src={src}
           loop
           muted
